ce778ce65e1f031829f12213a0f31ac438986298
Eduardmantilla <Eduardmantilla@hotmail.com>
2025-11-06 22:10:48 -0500

    Se realizo el primer ejercicio con sus respectivos comentarios

---
+++ app.js
@@ -0,0 +1,8 @@
+
+for (let i = 1; i <= 20; i++) {
+    // Verificar si el número es múltiplo de 3
+    if (i % 3 === 0) {
+    // imprime el numero si es multiplo de 3
+        console.log(i);
+    }
+}
